db4c3d5b4de7babef3a61cbc3890bd756b9ee418
Elena <elenabougaran@gmail.com>
2025-07-15 11:35:43 +0200

    Commit initial

---
+++ CarCare/CarCare/CarCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCareApp.swift
+//  CarCare
+//
+//  Created by Ordinateur elena on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CarCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
