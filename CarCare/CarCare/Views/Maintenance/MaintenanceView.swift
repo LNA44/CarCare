67cdd777e0771bc6d56505829b9670f665cf28c8
Elena <elenabougaran@gmail.com>
2025-09-07 15:56:44 +0200

    Coloration de la liste

--- CarCare/CarCare/Views/Maintenance/MaintenanceView.swift
+++ CarCare/CarCare/Views/Maintenance/MaintenanceView.swift
@@ -33,97 +33,105 @@ struct MaintenanceView: View {
 	//MARK: -Body
 	var body: some View {
 		let sortedKeys = VM.sortedMaintenanceKeys(from: maintenanceVM.maintenances)
-		VStack(spacing: 20) {
-			VStack {
-				if let lastMaintenanceByType = lastMaintenanceByType {
-					List {
-						Section(header: Text(NSLocalizedString("maintenances_to_come", comment: "Title for upcoming maintenance section"))
-							.font(.system(size: 27, weight: .bold, design: .rounded))
-							.foregroundColor(Color("TextColor"))
-							.textCase(nil)) {
-								ForEach(sortedKeys, id: \.self) { type in
-									if let maintenance = lastMaintenanceByType[type] {
-										NavigationLink(destination: MaintenanceDetailsView(bikeVM: bikeVM, maintenanceVM: maintenanceVM, maintenanceID: maintenance.id)) {
-											ToDoMaintenanceRow(VM: VM, maintenanceType: type)
+		ZStack {
+			Color("BackgroundColor") // fond principal
+					.ignoresSafeArea()  // couvre tout l’écran
+			VStack(spacing: 20) {
+				VStack {
+					if let lastMaintenanceByType = lastMaintenanceByType {
+						List {
+							Section(header: Text(NSLocalizedString("maintenances_to_come", comment: "Title for upcoming maintenance section"))
+								.font(.system(size: 27, weight: .bold, design: .rounded))
+								.foregroundColor(Color("TextColor"))
+								.textCase(nil)) {
+									ForEach(sortedKeys, id: \.self) { type in
+										if let maintenance = lastMaintenanceByType[type] {
+											NavigationLink(destination: MaintenanceDetailsView(bikeVM: bikeVM, maintenanceVM: maintenanceVM, maintenanceID: maintenance.id)) {
+												ToDoMaintenanceRow(VM: VM, maintenanceType: type)
+											}
+											.listRowBackground(Color("MaintenanceHistoryColor"))
 										}
 									}
 								}
-							}
-						Section(header: Text(NSLocalizedString("completed_maintenances", comment: "Title for completed maintenance section"))
-							.font(.system(size: 27, weight: .bold, design: .rounded))
-							.foregroundColor(Color("TextColor"))
-							.textCase(nil)) {
-								ForEach(maintenanceVM.maintenances.reversed(), id: \.self) { maintenance in
-									DoneMaintenanceRow(maintenance: maintenance)
-								}
-								.onDelete { offsets in
-									// Convertir offsets de la vue inversée en indices du tableau original
-									let realOffsets = offsets.map { maintenanceVM.maintenances.count - 1 - $0 }
+							Section(header: Text(NSLocalizedString("completed_maintenances", comment: "Title for completed maintenance section"))
+								.font(.system(size: 27, weight: .bold, design: .rounded))
+								.foregroundColor(Color("TextColor"))
+								.textCase(nil)) {
+									ForEach(maintenanceVM.maintenances.reversed(), id: \.self) { maintenance in
+										DoneMaintenanceRow(maintenance: maintenance)
+											.listRowBackground(Color("MaintenanceHistoryColor"))
+									}
 									
-									realOffsets.forEach { index in
-										let maintenance = maintenanceVM.maintenances[index]
-										maintenanceVM.deleteOneMaintenance(maintenance: maintenance, bikeType: bikeVM.bikeType)
+									.onDelete { offsets in
+										// Convertir offsets de la vue inversée en indices du tableau original
+										let realOffsets = offsets.map { maintenanceVM.maintenances.count - 1 - $0 }
+										
+										realOffsets.forEach { index in
+											let maintenance = maintenanceVM.maintenances[index]
+											maintenanceVM.deleteOneMaintenance(maintenance: maintenance, bikeType: bikeVM.bikeType)
+										}
 									}
 								}
-							}
-					}
-				} else {
-					ZStack {
-						VStack(alignment: .leading, spacing: 40) {
-							
-							Text(NSLocalizedString("maintenances_to_come", comment: "Title for upcoming maintenance section"))
-								.font(.system(size: 27, weight: .bold, design: .rounded))
-								.foregroundColor(Color("TextColor"))
-							
-							Text(NSLocalizedString("completed_maintenances", comment: "Title for completed maintenance section"))
-								.font(.system(size: 27, weight: .bold, design: .rounded))
-								.foregroundColor(Color("TextColor"))
-							Spacer()
 						}
-						.frame(maxWidth: .infinity, alignment: .leading)
-						.padding(.top, 20)
-						.padding(.leading, 10)
-						
-						VStack {
-							HStack(spacing: 10) {
-								Image(systemName: "exclamationmark.triangle.fill")
-								Text(NSLocalizedString("record_maintenance_instructions_key", comment: "Instructions pour enregistrer un entretien"))
-
+						.listStyle(.plain)
+					} else {
+						ZStack {
+							VStack(alignment: .leading, spacing: 40) {
+								
+								Text(NSLocalizedString("maintenances_to_come", comment: "Title for upcoming maintenance section"))
+									.font(.system(size: 27, weight: .bold, design: .rounded))
+									.foregroundColor(Color("TextColor"))
+								
+								Text(NSLocalizedString("completed_maintenances", comment: "Title for completed maintenance section"))
+									.font(.system(size: 27, weight: .bold, design: .rounded))
+									.foregroundColor(Color("TextColor"))
+								Spacer()
+							}
+							.frame(maxWidth: .infinity, alignment: .leading)
+							.padding(.top, 20)
+							.padding(.leading, 10)
+							
+							VStack {
+								HStack(spacing: 10) {
+									Image(systemName: "exclamationmark.triangle.fill")
+									Text(NSLocalizedString("record_maintenance_instructions_key", comment: "Instructions pour enregistrer un entretien"))
 									
+									
+								}
+								.padding(.horizontal, 10)
+								.padding(10)
+								
 							}
 							.padding(.horizontal, 10)
-							.padding(10)
-							
+							.background(Color("ToDoColor"))
+							.cornerRadius(10)
+							.overlay (
+								RoundedRectangle(cornerRadius: 10)
+									.stroke(Color.red, lineWidth: 2))
 						}
-						.padding(.horizontal, 10)
-						.background(Color("ToDoColor"))
-						.cornerRadius(10)
-						.overlay (
-							RoundedRectangle(cornerRadius: 10)
-								.stroke(Color.red, lineWidth: 2))
 					}
 				}
 			}
-		}
-		.alert(isPresented: $maintenanceVM.showAlert) {
-			Alert(
-				title: Text(NSLocalizedString("error_title", comment: "Title for error alert")),
-				message: Text(maintenanceVM.error?.localizedDescription ?? NSLocalizedString("unknown_error", comment: "Fallback unknown error")),
-				dismissButton: .default(Text("OK")) {
-					bikeVM.showAlert = false
-					bikeVM.error = nil
-				}
-			)
-		}
-		.alert(isPresented: $VM.showAlert) {
-			Alert(
-				title: Text(NSLocalizedString("error_title", comment: "Title for error alert")),
-				message: Text(VM.error?.localizedDescription ?? NSLocalizedString("unknown_error", comment: "Fallback unknown error")),
-				dismissButton: .default(Text("OK")) {
-					bikeVM.showAlert = false
-					bikeVM.error = nil
-				}
-			)
+			.alert(isPresented: $maintenanceVM.showAlert) {
+				Alert(
+					title: Text(NSLocalizedString("error_title", comment: "Title for error alert")),
+					message: Text(maintenanceVM.error?.localizedDescription ?? NSLocalizedString("unknown_error", comment: "Fallback unknown error")),
+					dismissButton: .default(Text("OK")) {
+						bikeVM.showAlert = false
+						bikeVM.error = nil
+					}
+				)
+			}
+			.alert(isPresented: $VM.showAlert) {
+				Alert(
+					title: Text(NSLocalizedString("error_title", comment: "Title for error alert")),
+					message: Text(VM.error?.localizedDescription ?? NSLocalizedString("unknown_error", comment: "Fallback unknown error")),
+					dismissButton: .default(Text("OK")) {
+						bikeVM.showAlert = false
+						bikeVM.error = nil
+					}
+				)
+			}
 		}
 	}
 }
